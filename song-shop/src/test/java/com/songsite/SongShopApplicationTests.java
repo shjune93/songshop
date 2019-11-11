916cbda7c5f5557f66fb0dbd01b1b7c82e2af0ab
shjune93 <shjune93@gmail.com>
2019-11-11 17:50:18 +0900

    빌드명 변경을 톻한 package 에러 제거

---
+++ song-shop/src/test/java/com/songsite/SongShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
